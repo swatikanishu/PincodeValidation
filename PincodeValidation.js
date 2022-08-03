04fafa342cd3c6a7b760fd3a1be097ed93cfb49b
swatikanishu <nishuswatika@gmail.com>
2022-08-04 02:40:33 +0530

    MODIFIED:Allow space between pinCode Digits

--- PincodeValidation.js
+++ PincodeValidation.js
@@ -1,5 +1,5 @@
 console.log("Welcome To PinCode Validation ");
-const pincodeRegex = RegExp("^[0-9]{6}$");
+const pincodeRegex = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
 
 function validatePincode(pincode){
     if(pincodeRegex.test(pincode))
@@ -10,7 +10,7 @@ function validatePincode(pincode){
 
 try{
     validatePincode("400088");
-    validatePincode("40088B");
+    validatePincode("400 088");
 }
 catch(e){
     console.error(e);
